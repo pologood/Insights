717c9120546686533e26c365340f8317957c58cd
selvam12794 <selvam.ramasamy@cognizant.com>
2019-04-23 19:51:26 +0530

    #164367775 search filter added for the relationship name

--- PlatformUI3/src/app/com/cognizant/devops/platformui/modules/relationship-builder/relationship-builder.component.ts
+++ PlatformUI3/src/app/com/cognizant/devops/platformui/modules/relationship-builder/relationship-builder.component.ts
@@ -4,6 +4,7 @@ import { ShowJsonDialog } from '@insights/app/modules/relationship-builder/show-
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { RelationLabel } from '@insights/app/modules/relationship-builder/relationship-builder.label';
 import { from } from 'rxjs';
+import {MatTableDataSource} from '@angular/material';
 //import { Control} from '@angular/common';
 @Component({
   selector: 'app-relationship-builder',
@@ -89,7 +90,7 @@ export class RelationshipBuilderComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  
   async dataDictionaryInfo() {
     try {
       // Loads Agent , Data Component and Services
@@ -206,9 +207,27 @@ export class RelationshipBuilderComponent implements OnInit {
 
   }
 
-
-
-
+  searchTable() {
+    var input, filter, found, table, tr, td, i, j;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td");
+        for (j = 0; j < td.length; j++) {
+            if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                found = true;
+            }
+        }
+        if (found) {
+            tr[i].style.display = "";
+            found = false;
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
+}
   getCorrelation() {
     try {
       this.relationDataSourceNeo4j = [];
